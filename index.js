b211893f6e2af35cf0d5f8b62337a99bbddcec00
Jacquan Brooks <jacquanb5@gmail.com>
2022-01-13 17:51:16 -0600

    Fixing code

--- index.js
+++ index.js
@@ -1,9 +1,23 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(ride){
   // Write your code here!
+  if (ride <= 400) {
+    ride = "This one is on me!";
+  } else if (ride <= 2499) {
+    ride = "I will gladly take your thirty bucks.";
+  } else {
+    ride = "No can do.";
+  }
+  return ride
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(NYC){
   // Write your code here!
+
+  const NYC = city;
+  
+  const city =  NYC === ternaryCheckCity ? "Ok, sounds good." : "No Go";
+
+  city
 }
 
 function switchOnCharmFromTip(){
